6c9e7bc0afac4537c25723c2d37aebf169e853e2
Danny-1211 <danny21316@gmail.com>
2024-12-07 15:34:16 +0800

    test

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -259,7 +259,7 @@ SELECT * FROM "USER" LIMIT 3;
     INNER JOIN "COACH_LINK_SKILL" AS CLK ON C.id = CLK.coach_id
     INNER JOIN "SKILL" AS S ON S.id = CLK.skill_id
     WHERE S.name = '重訓'
-    ORDER BY experience_years DESC;
+    ORDER BY c.experience_years DESC;
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
     -- SELECT MAX(S.name) AS 專長名稱,Count(CLK.coach_id) AS coach_total
